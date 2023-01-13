65552439317ebba4f9ce6d238e41e8d5e560c019
itsdiku <you@example.com>
2023-01-13 22:25:44 +0530

    Added API and StyleSheets

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import "./App.css";
-import logo from "./logo.svg";
+import { useState } from "react";
+import React from 'react';
+import axios from "axios";
+import './index.css';
+
 
 function App() {
+  const[setup, setSetup] = useState('Jokes Setup')
+  const[punchline, setPunchline] = useState('Jokes Punchline')
+  const handleClick = () => {
+    const options = {method: 'GET', url: 'https://official-joke-api.appspot.com/random_joke'};
+    axios.request(options)
+    .then(function (response) 
+      {
+        setSetup(response.data.setup);
+        setPunchline(response.data.punchline);
+      }
+    );
+    
+  }
+
+
   return (
-    <div className="App" role="main">
-      <article className="App-article">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </article>
+    <div className="app">
+      <div className="container">
+        <div className="setup">{ setup }</div>
+        <div className="punchline">{ punchline }</div>
+        <button onClick = {handleClick} className='btn'>Random Jokes!</button>
+      </div>
     </div>
   );
 }
